c3ae742a96bb2093dabebd23c9aeddaa177ed4e4
Super-Nova <piyusht189@gmail.com>
2016-02-23 00:21:41 +0530

    Arindam Session 2 Assignment 2 - By Arindam

--- app/src/main/java/com/arindam/familylist/CustomList.java
+++ app/src/main/java/com/arindam/familylist/CustomList.java
@@ -8,12 +8,23 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
 public class CustomList extends ArrayAdapter<String> {
 
     private final Activity context;
     private final String[] name;
     private final String[] relation;
     private final Integer[] imageId;
+    public Firebase myFirebaseRef;
+    TextView txtname;
+    TextView txtrelation;
+    ImageView imageView;
+    int pos;
 
     public CustomList(Activity context, String[] name, String[] relation, Integer[] imageId) {
         super(context, R.layout.list_single, name);
@@ -21,20 +32,36 @@ public class CustomList extends ArrayAdapter<String> {
         this.relation = relation;
         this.name = name;
         this.imageId = imageId;
+        Firebase.setAndroidContext(getContext());
+        myFirebaseRef = new Firebase("https://relationalmanac.firebaseio.com/");
 
     }
     @Override
     public View getView(int position, View view, ViewGroup parent) {
         LayoutInflater inflater = context.getLayoutInflater();
         View rowView= inflater.inflate(R.layout.list_single, null, true);
-        TextView txtname = (TextView)rowView.findViewById(R.id.txt1);
-        TextView txtrelation = (TextView)rowView.findViewById(R.id.txt2);
-        ImageView imageView = (ImageView)rowView.findViewById(R.id.img);
+        txtname = (TextView)rowView.findViewById(R.id.txt1);
+        txtrelation = (TextView)rowView.findViewById(R.id.txt2);
+        imageView = (ImageView)rowView.findViewById(R.id.img);
 
         txtname.setText(name[position]);
         txtrelation.setText(relation[position]);
         imageView.setImageResource(imageId[position]);
 
+        myFirebaseRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                txtname.setText(String.valueOf(dataSnapshot.child("Relations").child(String.valueOf(pos)).child("Name").getValue()));
+                txtrelation.setText(String.valueOf(dataSnapshot.child("Relations").child(String.valueOf(pos)).child("Relation").getValue()));
+                Picasso.with(getContext()).load(String.valueOf(dataSnapshot.child("Relations").child(String.valueOf(pos)).child("Image").getValue())).into(imageView);
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
         return rowView;
+
     }
 }
